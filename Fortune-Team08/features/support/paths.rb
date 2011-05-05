f160bbe4be8907b1e8002f8734348a938a932fb0
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-05-05 15:12:17 -0400

    Changed the paths.rb correctly

--- Fortune-Team08/features/support/paths.rb
+++ Fortune-Team08/features/support/paths.rb
@@ -12,6 +12,9 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+	  
+	when /fortunes/i
+		'/fortune/random/'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
